dc65f9d5b40c7a6415e15915927f14f2c51b0d0a
giraffedata <giraffedata@9d0c8265-081b-0410-96cb-a4ca84ce46f8>
2006-09-11 02:10:46 +0000

    Remove generated files
    git-svn-id: http://svn.code.sf.net/p/netpbm/code/trunk@48 9d0c8265-081b-0410-96cb-a4ca84ce46f8

--- lib/compile.h
+++
@@ -1,5 +0,0 @@
-/* This file tells the package when it was compiled */
-/* DO NOT EDIT - THIS FILE IS MAINTAINED AUTOMATICALLY */
-/* Created by the program 'stamp-date'  */
-#define COMPILE_TIME "Tue Aug 29 15:40:16 GMT 2006"
-#define COMPILED_BY "bryanh"
