d5f071d84763108e3e6d6f5a14622dbaeed0b9c2
giraffedata <giraffedata@9d0c8265-081b-0410-96cb-a4ca84ce46f8>
2007-09-29 18:41:46 +0000

    use 256 pixel map for palmcolor8.map
        palmcolor8.map is a map of 231 colors with an extra black pixel, sorted by
        color. It makes more sense to use a 256 color map of those 231 colors and
        25 black pixels, sorted by index (as per Palm's spec).
        
        Signed-off-by: Paul Bolle <pebolle@tiscali.nl>
    
    
    
    git-svn-id: http://svn.code.sf.net/p/netpbm/code/trunk@420 9d0c8265-081b-0410-96cb-a4ca84ce46f8

--- converter/other/pnmtopalm/gen_palm_colormap.c
+++ converter/other/pnmtopalm/gen_palm_colormap.c
@@ -1,9 +1,10 @@
 /* gen_palm_colormap.c - generate a ppm file containing the default Palm colormap
  *
- * Bill Janssen  <bill@janssen.org>
+ * Based on an earlier version by Bill Janssen  <bill@janssen.org>
  */
 
-#include "pnm.h"
+#include "ppm.h"
+#include "pm_c_util.h"
 
 #include "palm.h"
 
@@ -11,19 +12,33 @@ int
 main(int     argc,
      char ** argv) {
 
-    Colormap const defaultMap = palmcolor_build_default_8bit_colormap();
-
+    Colormap defaultMap;
     unsigned int i;
+    pixel pix;
     
-    printf("P3\n%d 1\n255\n", defaultMap->ncolors);
+    defaultMap = palmcolor_build_default_8bit_colormap();
+    qsort (defaultMap->color_entries, defaultMap->ncolors,
+           sizeof(Color_s), palmcolor_compare_indices);
+
+    ppm_writeppminit(stdout, 256, 1, 255, TRUE);
 
     for (i = 0; i < defaultMap->ncolors; ++i) {
         Color_s const current = defaultMap->color_entries[i];
 
-        printf("%u %u %u\n",
-               (unsigned char)(current >> 16),
-               (unsigned char)(current >>  8),
-               (unsigned char)(current >>  0));
+        PPM_ASSIGN(pix,
+                   (current >> 16) & 0xff,
+                   (current >>  8) & 0xff,
+                   (current >>  0) & 0xff);
+
+        ppm_writeppmrow(stdout, &pix, 1, 255, TRUE);
+    }
+
+    /* palmcolor_build_default_8bit_colormap() builds a map of the 231 default
+     * palm colors and 1 extra black pixel. Add another 24 extra black pixels
+     * as per spec. */
+    PPM_ASSIGN(pix, 0, 0, 0);
+    for (i = 0; i < 256 - defaultMap->ncolors; ++i) {
+        ppm_writeppmrow(stdout, &pix, 1, 255, TRUE);
     }
 
     return 0;
