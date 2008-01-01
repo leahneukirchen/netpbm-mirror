b2cabe67ae053fe2b734c0bbecc434246a9692d5
giraffedata <giraffedata@9d0c8265-081b-0410-96cb-a4ca84ce46f8>
2008-01-01 17:35:34 +0000

    Add sleepN()
    git-svn-id: http://svn.code.sf.net/p/netpbm/code/trunk@540 9d0c8265-081b-0410-96cb-a4ca84ce46f8

---
+++ lib/util/nsleep.c
@@ -0,0 +1,27 @@
+#ifdef WIN32
+  #include <windows.h>
+  #include <process.h>
+#else
+  #include <unistd.h>
+#endif
+
+#include "nsleep.h"
+
+
+
+void
+sleepN(unsigned int const milliseconds) {
+
+#ifdef WIN32
+    SleepEx(milliseconds, true);
+#else
+
+    /* We could use usleep() here if millisecond resolution is really
+       important, but since Netpbm has no need for it today, we don't
+       want to deal with the possibility that usleep() doesn't exist.
+       08.08.01.
+    */
+
+    sleep((milliseconds + 999)/1000);
+#endif
+}
