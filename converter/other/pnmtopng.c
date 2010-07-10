64085f3a76eb6db2c14e52ccb06301f6a5807b77
giraffedata <giraffedata@9d0c8265-081b-0410-96cb-a4ca84ce46f8>
2010-07-10 01:38:01 +0000

    Release 10.35.76
    git-svn-id: http://svn.code.sf.net/p/netpbm/code/super_stable@1263 9d0c8265-081b-0410-96cb-a4ca84ce46f8

--- converter/other/pnmtopng.c
+++ converter/other/pnmtopng.c
@@ -2772,8 +2772,8 @@ displayVersion() {
     */
     fprintf(stderr, "   Compiled with libpng %s.\n",
             PNG_LIBPNG_VER_STRING);
-    fprintf(stderr, "   Compiled with zlib %s; using zlib %s.\n",
-            ZLIB_VERSION, zlib_version);
+    fprintf(stderr, "   Pnmtopng (not libpng) compiled with zlib %s.\n",
+            ZLIB_VERSION);
     fprintf(stderr, "\n");
 }
 
