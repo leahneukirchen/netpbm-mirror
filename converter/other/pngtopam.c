2096d552041fff92edef9ec3a6a5a6c63d210287
giraffedata <giraffedata@9d0c8265-081b-0410-96cb-a4ca84ce46f8>
2013-07-05 17:46:33 +0000

    Release 10.63.01
    git-svn-id: http://svn.code.sf.net/p/netpbm/code/advanced@1974 9d0c8265-081b-0410-96cb-a4ca84ce46f8

--- converter/other/pngtopam.c
+++ converter/other/pngtopam.c
@@ -327,6 +327,9 @@ reader_createAllAtOnce(struct pngx * const pngxP,
 
    The Reader object reads the PNG at construction time, stores the entire
    raster, and hands it out as you call reader_read().
+
+   It is essential that *pngxP be already fully set up to read the image
+   (all options set).
 -----------------------------------------------------------------------------*/
     Reader * readerP;
 
@@ -1441,9 +1444,6 @@ convertpng(FILE *             const ifP,
     if (verbose)
         dumpPngInfo(pngxP);
 
-    rasterReaderP = cmdline.byrow ? 
-        reader_createRowByRow(pngxP, ifP) : reader_createAllAtOnce(pngxP, ifP);
-
     if (cmdline.time)
         showTime(pngxP);
     if (tfP)
@@ -1468,6 +1468,9 @@ convertpng(FILE *             const ifP,
 
     determineOutputType(pngxP, cmdline.alpha, bgColor, pngxP->maxval,
                         &pam.format, &pam.depth, pam.tuple_type);
+
+    rasterReaderP = cmdline.byrow ? 
+        reader_createRowByRow(pngxP, ifP) : reader_createAllAtOnce(pngxP, ifP);
 
     writeNetpbm(&pam, pngxP, rasterReaderP, bgColor,
                 cmdline.alpha, totalgamma);
@@ -1514,3 +1517,6 @@ main(int argc, const char *argv[]) {
 
     return errorLevel;
 }
+
+
+
