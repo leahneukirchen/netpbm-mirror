877641742255c9eeb82438eb54fd772f65e61e84
giraffedata <giraffedata@9d0c8265-081b-0410-96cb-a4ca84ce46f8>
2021-04-08 02:13:49 +0000

    Release 10.94.02
    git-svn-id: http://svn.code.sf.net/p/netpbm/code/advanced@4084 9d0c8265-081b-0410-96cb-a4ca84ce46f8

--- lib/util/randsysrandom.c
+++ lib/util/randsysrandom.c
@@ -1,9 +1,14 @@
+/* Implementation note: Mingw/Windows does not provide POSIX 'random', so
+   netpbm/pm_config.h makes that a macro for POSIX 'rand' on that platform
+*/
+
 #define _DEFAULT_SOURCE /* New name for SVID & BSD source defines */
 #define _XOPEN_SOURCE 500  /* Make sure random() is in stdlib.h */
 #define _BSD_SOURCE  /* Make sure random() is in stdlib.h */
 
 #include <stdlib.h>
 
+#include "netpbm/pm_config.h"
 #include "netpbm/rand.h"
 
 static void
