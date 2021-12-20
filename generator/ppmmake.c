8abadd9e1e0e768b274532b6728bcc83415f9fb6
giraffedata <giraffedata@9d0c8265-081b-0410-96cb-a4ca84ce46f8>
2021-12-20 01:15:07 +0000

    whitespace
    git-svn-id: http://svn.code.sf.net/p/netpbm/code/trunk@4213 9d0c8265-081b-0410-96cb-a4ca84ce46f8

--- generator/ppmmake.c
+++ generator/ppmmake.c
@@ -31,11 +31,11 @@ static void
 parseCommandLine(int argc, char ** argv,
                  struct cmdlineInfo * const cmdlineP) {
 /*----------------------------------------------------------------------------
-  Convert program invocation arguments (argc,argv) into a format the 
+  Convert program invocation arguments (argc,argv) into a format the
   program can use easily, struct cmdlineInfo.  Validate arguments along
   the way and exit program with message if invalid.
 
-  Note that some string information we return as *cmdlineP is in the storage 
+  Note that some string information we return as *cmdlineP is in the storage
   argv[] points to.
 -----------------------------------------------------------------------------*/
     optEntry * option_def;
