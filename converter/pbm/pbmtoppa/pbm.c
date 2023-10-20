cfaacab456a42c7faeca432ef6c1db93670a61cf
giraffedata <giraffedata@9d0c8265-081b-0410-96cb-a4ca84ce46f8>
2023-10-20 18:21:01 +0000

    cleanup
    git-svn-id: http://svn.code.sf.net/p/netpbm/code/trunk@4768 9d0c8265-081b-0410-96cb-a4ca84ce46f8

--- converter/pbm/pbmtoppa/pbm.c
+++ converter/pbm/pbmtoppa/pbm.c
@@ -64,15 +64,15 @@ make_pbm_stat(pbm_stat * const pbmStatP,
                     if (pbmStatP->width < 0) {
                         pm_message("Image has negative width");
                         retval = 0;
-                    } else if (pbmStatP->width > INT_MAX/2) {
-                        pm_message("Uncomputeably large width: %d",
+                    } else if (pbmStatP->width > INT_MAX/2 - 10) {
+                        pm_message("Uncomputably large width: %d",
                                    pbmStatP->width);
                         retval = 0;
                     } else if (pbmStatP->height < 0) {
                         pm_message("Image has negative height");
                         retval = 0;
-                    } else if (pbmStatP->height > INT_MAX/2) {
-                        pm_message("Uncomputeably large height: %d",
+                    } else if (pbmStatP->height > INT_MAX/2 - 10) {
+                        pm_message("Uncomputably large height: %d",
                                    pbmStatP->height);
                         retval = 0;
                     } else
