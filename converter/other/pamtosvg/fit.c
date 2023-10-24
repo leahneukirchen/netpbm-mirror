9f1d2b3d257e1e6d68f9e56778e81fc71531bf7e
giraffedata <giraffedata@9d0c8265-081b-0410-96cb-a4ca84ce46f8>
2023-10-24 16:59:29 +0000

    Fix uninitialized variable from recent cleanup
    git-svn-id: http://svn.code.sf.net/p/netpbm/code/trunk@4775 9d0c8265-081b-0410-96cb-a4ca84ce46f8

--- converter/other/pamtosvg/fit.c
+++ converter/other/pamtosvg/fit.c
@@ -1284,7 +1284,8 @@ computeCX(Curve *                 const curveP,
 
     unsigned int pointSeq;
 
-    cP->beg.beg = 0.0; cP->beg.end = 0.0; cP->end.end = 0.0;/* initial value */
+    cP->beg.beg = 0.0; cP->beg.end = 0.0; cP->end.beg = 0.0; cP->end.end = 0.0;
+        /* initial value */
 
     xP->beg = 0.0; xP->end = 0.0; /* initial value */
 
@@ -1800,8 +1801,8 @@ divideAndFit(Curve *                   const curveP,
 
 static spline_list_type *
 fitWithLeastSquares(Curve *                   const curveP,
-                    Vector               const begSlope,
-                    Vector               const endSlope,
+                    Vector                    const begSlope,
+                    Vector                    const endSlope,
                     const fitting_opts_type * const fittingOptsP,
                     at_exception_type *       const exceptionP) {
 /*----------------------------------------------------------------------------
