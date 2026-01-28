52ebd8bf01450705ebcc7a78d31319a26585ac51
giraffedata <giraffedata@9d0c8265-081b-0410-96cb-a4ca84ce46f8>
2026-01-28 19:44:27 +0000

    Release 11.02.21
    git-svn-id: http://svn.code.sf.net/p/netpbm/code/stable@5157 9d0c8265-081b-0410-96cb-a4ca84ce46f8

--- generator/pbmtext.c
+++ generator/pbmtext.c
@@ -98,7 +98,7 @@ textFmCmdLine(int argc, const char ** argv) {
                  "wide characters of text", totaltextsize);
 
     for (i = 0; i < totaltextsize + 1; ++i)
-        wtext[i] = (PM_WCHAR) text[i];
+        wtext[i] = (PM_WCHAR) (unsigned char)text[i];
 
     free(text);
 
