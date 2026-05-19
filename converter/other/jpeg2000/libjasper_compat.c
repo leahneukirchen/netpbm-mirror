0776bc0bfca413832579ce395965abd34c8e18ff
giraffedata <giraffedata@9d0c8265-081b-0410-96cb-a4ca84ce46f8>
2026-05-19 15:51:30 +0000

    fix compiler warning/error with real Jasper library
    git-svn-id: http://svn.code.sf.net/p/netpbm/code/trunk@5189 9d0c8265-081b-0410-96cb-a4ca84ce46f8

--- converter/other/jpeg2000/libjasper_compat.c
+++ converter/other/jpeg2000/libjasper_compat.c
@@ -3,30 +3,11 @@
 #include "jasper/jasper.h"
 #include "jasper/jas_image.h"
 
-#ifndef JAS_HAVE_PMJAS_IMAGE_DECODE
 
-/* The Netpbm build has been modified to use original libjasper instead of the
-   fork of it in Netpbm.  That means libjasper has 'jas_image_decode' instead
-   of 'pmjas_image_decode' and Netpbm programs that use libjasper have to use
-   the former, via this compatibility wrapper.
+/* This is for stuff to create backward compatibility with older Jasper
+   libraries and with the distributed Jasper library.  See
+   libjasper_compat.h.
+
+   'pmjas_image_decode' used to be here, but now is defined inline in
+   libjasper_compat.h.
 */
-
-void
-pmjas_image_decode(jas_stream_t * const in,
-                   int            const fmtArg,
-                   const char *   const optstr,
-                   jas_image_t ** const imagePP,
-                   const char **  const errorP) {
-
-    jas_image_t * const jasperP = jas_image_decode(in, fmtArg, optstr);
-
-    if (jasperP) {
-        *imagePP = jasperP;
-        *errorP  = errorP;
-    } else {
-        pm_asprintf(errorP, "Failed.  Details may have been written to "
-                    "Standard Error");
-    }
-}
-
-#endif
