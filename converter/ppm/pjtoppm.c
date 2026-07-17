92de08fad479a853e29492984b24b245152fb960
giraffedata <giraffedata@9d0c8265-081b-0410-96cb-a4ca84ce46f8>
2026-07-17 14:26:47 +0000

    fix typo in messages
    git-svn-id: http://svn.code.sf.net/p/netpbm/code/trunk@5265 9d0c8265-081b-0410-96cb-a4ca84ce46f8

--- converter/ppm/pjtoppm.c
+++ converter/ppm/pjtoppm.c
@@ -307,7 +307,7 @@ main(int argc, const char ** argv) {
                 case 'R':
                     break;  /* set resolution */
                 default:
-                    pm_message("uninmplemented <ESC>*%c%d%c", cmd, val, c);
+                    pm_message("unimplemented <ESC>*%c%d%c", cmd, val, c);
                     break;
                 }
                 break;
@@ -352,7 +352,7 @@ main(int argc, const char ** argv) {
                 case 'H':
                     break;  /* set deci width */
                 default:
-                    pm_message("uninmplemented <ESC>*%c%c command class", cmd, c);
+                    pm_message("unimplemented <ESC>*%c%c command class", cmd, c);
                     break;
                 }
                 break;
@@ -404,7 +404,7 @@ main(int argc, const char ** argv) {
                     }
                     break;
                 default:
-                    pm_message("uninmplemented <ESC>*%c%d%c", cmd, val, c);
+                    pm_message("unimplemented <ESC>*%c%d%c", cmd, val, c);
                     break;
                 }
                 break;
@@ -452,11 +452,11 @@ main(int argc, const char ** argv) {
                                  "for computation", row);
                 } break;
                 default:
-                    pm_message("uninmplemented <ESC>*p%d%c", val, c);
+                    pm_message("unimplemented <ESC>*p%d%c", val, c);
                     break;
                 }
             default:
-                pm_message("uninmplemented <ESC>*%c%c", cmd, c);
+                pm_message("unimplemented <ESC>*%c%c", cmd, c);
                 break;
              }
         } /* case */
